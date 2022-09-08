fc14b3111a37026eebd662099df3582d388f0c11
Yunus Emre Eryarsoy <eryaroyyunusemre@gmail.com>
2022-09-08 18:03:05 +0300

    Add project files.

---
+++ PhoneBook/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Yapmak Istediginiz Islemi Seciniz..");
+            Console.WriteLine("*******************************************");
+            Console.WriteLine("(1) Yeni Numara Kaydetmek");
+            Console.WriteLine("(2) Varolan Numarayı Silmek");
+            Console.WriteLine("(3) Varolan Numarayı Güncelleme");
+            Console.WriteLine("(4) Rehberi Listelemek");
+            Console.WriteLine("(5) Rehberde Arama Yapmak");
+            
+            string secim = Console.ReadLine();
+            Islemler islem = new Islemler();
+            switch (secim)
+            {
+                case "1":
+                    islem.KisiAdd();                   
+                    break;
+                case "2":
+                    islem.KisiDelete();
+                    break;
+                case "3":
+                    islem.KisiGüncelleme();
+                    break;
+                case "4":
+                    islem.KisiListeleme();
+                    break;
+                case "5":
+                    islem.KisiArama();
+                    break;
+
+            }
+        }
+    }
+}
